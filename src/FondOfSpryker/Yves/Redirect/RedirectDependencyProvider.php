8a4e10957bd6a79f951270538aa8e758a04889a2
Daniel Kemna <danielkemna@t-online.de>
2019-12-12 14:33:00 +0100

    add session managment for localization

--- src/FondOfSpryker/Yves/Redirect/RedirectDependencyProvider.php
+++ src/FondOfSpryker/Yves/Redirect/RedirectDependencyProvider.php
@@ -8,6 +8,7 @@ use Spryker\Yves\Kernel\Container;
 class RedirectDependencyProvider extends AbstractBundleDependencyProvider
 {
     public const CLIENT_STORE = 'CLIENT_STORE';
+    public const SESSION_STORE = 'SESSION_STORE';
 
     /**
      * @param \Spryker\Yves\Kernel\Container $container
@@ -17,6 +18,7 @@ class RedirectDependencyProvider extends AbstractBundleDependencyProvider
     public function provideDependencies(Container $container): Container
     {
         $container = $this->provideStoreClient($container);
+        $container = $this->provideSessionClient($container);
 
         return $container;
     }
@@ -30,6 +32,20 @@ class RedirectDependencyProvider extends AbstractBundleDependencyProvider
     {
         $container[static::CLIENT_STORE] = function (Container $container) {
             return $container->getLocator()->store()->client();
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Yves\Kernel\Container $container
+     *
+     * @return \Spryker\Yves\Kernel\Container
+     */
+    protected function provideSessionClient(Container $container): Container
+    {
+        $container[static::SESSION_STORE] = function (Container $container) {
+            return $container->getLocator()->session()->client();
         };
 
         return $container;
